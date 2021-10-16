33473c2e7fdd65c062b75750f1d7eb1922acdac8
caspari-svg <holguinsebastian9@gmail.com>
2021-10-15 22:52:51 -0400

    Optimized code for mandelbrot.py. Also finished the markov_chain file. Not sure if it's correct..

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,23 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy import newaxis
+
+
+# newaxis lets me go up an axis when dealing with the array
+# so I don't have to manually change the size depending on n.
 
 
 def mandelbrot(n, N_max, threshold):
     xmatrix = np.linspace(-2, 1, n)
+    # xmatrix uses linspace to create an array of min = -2, max = 1, of n iterations.
     ymatrix = np.linspace(-1.5, 1.5, n)
-    c = xmatrix[:, newaxis] + 1j * ymatrix[newaxis, :]
+    # ymatrix uses linspace to create an array of min = -1.5, max = 1.5, of n iterations.
+    c = xmatrix[:, np.newaxis] + 1j * ymatrix[np.newaxis, :]
+    # getting variables from the start of the matrix, all the way to whatever size
+    # our array is. This is the equivalent of getting every index and creating
+    # our variable c.
     z = 0
-
     for i in range(N_max):
-        z = z**2 + c
+        z = z ** 2 + c
 
-    mask = (abs(z) < threshold)
-    return mask
+    array = (abs(z) < threshold)
+    return array
 
 
 mask = mandelbrot(600, 50, 500)
 
 plt.imshow(mask.T, extent=[-2, 1, -1.5, 1.5])
 plt.gray()
+plt.show()
 plt.savefig('mandelbrot.png')
