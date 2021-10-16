fde83a7c0d01f9c5cf9aa2ba2eaab6290862bbf2
caspari-svg <holguinsebastian9@gmail.com>
2021-10-15 22:09:05 -0400

    Refactored some variable names for description.

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,6 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from numpy import newaxis
+import matplotlib.pyplot as plt
 
 
 def mandelbrot(n, N_max, threshold):
@@ -8,14 +8,16 @@ def mandelbrot(n, N_max, threshold):
     ymatrix = np.linspace(-1.5, 1.5, n)
     c = xmatrix[:, newaxis] + 1j * ymatrix[newaxis, :]
     z = 0
-    for j in range(N_max):
+
+    for i in range(N_max):
         z = z**2 + c
+
     mask = (abs(z) < threshold)
     return mask
 
 
-mandelbrot_set = mandelbrot(600, 50, 500)
+mandelbrot = mandelbrot(600, 50, 500)
 
-plt.imshow(mandelbrot_set.T, extent=[-2, 1, -1.5, 1.5])
+plt.imshow(mandelbrot.T, extent=[-2, 1, -1.5, 1.5])
 plt.gray()
 plt.savefig('mandelbrot.png')
